4b7aa729f7c6e07bcf78d70fcb89101af0a864f0
jesser360 <jesser360@gmail.com>
2017-06-14 18:03:02 -0700

    swiping fx

--- app.js
+++ app.js
@@ -2,12 +2,57 @@ console.log('sanity check')
 $(document).ready(function() {
 
   $(function() {
-    $("#cosmic1").swipe( function(){
+    //Enable swiping...
+    var cosmicCounterRight = 0;
+    var cosmicCounterLeft = 0;
+    var totalCosmicCounter = 0;
+    $("#cosmic1").swipe( {
       //Generic swipe handler for all directions
-      // swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
-      //   $(this).text("You swiped " + direction );
-      console.log('swiped');
-    })
+      swipe:function(event, direction) {
+        if(direction == 'right' ){
+            if(totalCosmicCounter < -4 || totalCosmicCounter > 4){
+              totalCosmicCounter = 0;
+              cosmicCounterRight = 0;
+              cosmicCounterLeft = 0;
+          } else{
+              cosmicCounterRight ++;
+              totalCosmicCounter = cosmicCounterRight - cosmicCounterLeft;
+              console.log(totalCosmicCounter)
+          }
+        } else {
+            if(totalCosmicCounter < -4 || totalCosmicCounter > 4){
+              totalCosmicCounter = 0;
+              cosmicCounterRight = 0;
+              cosmicCounterLeft = 0;
+          } else{
+            cosmicCounterLeft ++;
+            totalCosmicCounter = cosmicCounterRight - cosmicCounterLeft;
+            console.log(totalCosmicCounter)
+          }
+        }
+        if(totalCosmicCounter == 0){
+        $("#cosmic1").attr("src","http://i.imgur.com/B2zSOfx.jpg");
+      } else if(totalCosmicCounter == 1 || totalCosmicCounter == -1){
+        $("#cosmic1").attr("src","http://i.imgur.com/X6xRuLp.jpg");
+      } else if(totalCosmicCounter== 2 || totalCosmicCounter == -2){
+        $("#cosmic1").attr("src","http://i.imgur.com/FfJpwcg.jpg");
+      } else if(totalCosmicCounter == 3 || totalCosmicCounter == -3){
+        $("#cosmic1").attr("src","http://i.imgur.com/gfSC0kC.jpg");
+      } else if(totalCosmicCounter == 4 || totalCosmicCounter == -4){
+        $("#cosmic1").attr("src","http://i.imgur.com/qr5OnKD.jpg");
+      } else if(totalCosmicCounter == 5 || totalCosmicCounter == -5){
+        $("#cosmic1").attr("src","http://i.imgur.com/Sp3f0Ad.jpg");
+      }
+      },
+      //Default is 75px, set to 0 for demo so any distance triggers swipe
+       threshold:0
     });
+  });
+
+
+  // $("#cosmic1").click(function(){
+  //   console.log('clicked')
+  // })
+
 
 });//close doc ready
